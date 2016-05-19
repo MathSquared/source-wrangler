43a141ec8b96b7f0718c681ccaee4c40c7c6a4f0
MathSquared <MathSquared@users.noreply.github.com>
2016-05-18 22:49:56 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="source-wrangler",
+    version="0.1",
+    author="MathSquared",
+    description="A source manager for research projects (not source code)",
+    license="MIT",
+    packages=["source-wrangler"]
+)
