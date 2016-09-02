a1a1c7adc28db88b7e680ff57e09c81f26504d95
MathSquared <MathSquared@users.noreply.github.com>
2016-09-01 23:55:00 -0500

    Add command module

---
+++ tests/command_test.py
@@ -0,0 +1,27 @@
+import unittest
+
+from sourcewrangler import command
+
+class TestCommandRetriever(unittest.TestCase):
+    
+    def test_incomplete(self):
+        def should_break():
+            @command.register_command("test_incomplete")
+            class TestIncompleteCommand(object):
+                pass
+
+        self.assertRaises(command.ImproperlyDefinedCommandError, should_break)
+
+    def test_basic(self):
+        @command.register_command("test_basic")
+        class TestBasicCommand(object):
+            @staticmethod
+            def specify_args(argparse):
+                pass
+
+            def run(self, args):
+                pass
+
+        self.assertEqual(TestBasicCommand.name, "test_basic")
+        self.assertTrue(command.has_command("test_basic"))
+        self.assertEqual(command.get_command("test_basic"), TestBasicCommand)
